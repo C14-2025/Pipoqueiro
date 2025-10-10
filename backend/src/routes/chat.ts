f3d8f9d5d5218b859e6c56dd78d119d1804844b5
Davi Padula Rabelo <160641658+DaviPRabelo@users.noreply.github.com>
2025-10-10 16:03:06 -0300

    feat: adicionando chave api ao .env

---
+++ backend/src/routes/chat.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import { chatController } from '../controllers/chatController';
+
+const router = Router();
+
+//Rota pública
+router.post('/', chatController.sendMessage);
+
+export default router;
